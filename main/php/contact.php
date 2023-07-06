1b8f3e753877602a49933f9b2c225a070b09f5a5
GamesFoundery <85865703+GamesFoundery@users.noreply.github.com>
2023-07-06 13:16:59 +0200

    Recreation of the site

---
+++ main/php/contact.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Jules Baud Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="css/contactstyle.css">
+</head>
+<body>
+	<div id="page">
+		<div id="header">
+			<div id="header-left">
+				<nav class="navBar">
+					<ul>
+						<li class="navBttn">
+							<a class="navLink" href="../index.html">
+								HOME
+							</a>
+						</li>
+						<li class="navBttn">
+							<a class="navLinkActual"href="#">
+								CONTACT
+							</a>
+						</li>				
+					</ul>
+				</nav>			
+			</div>
+
+			<div id="header-logo">
+				<img class="logoSite" src="img/logo_jules.png">
+			</div>
+
+			<div id="header-right">
+				<nav class="navBar">
+					<ul>
+						<li class="navBttn">
+							<a class="navLink" href="../portfolio.html">
+								PORTFOLIO
+							</a>
+						</li>
+						<li class="navBttn">
+							<a class="navLink" href="../games.html">
+								GAMES PROJECTS
+							</a>
+						</li>			
+					</ul>
+				</nav>			
+			</div>
+		</div>
+		<div id="core">
+			<div id="contactBox">
+				<div class="leftBox">
+					<div class="coordsBox">
+						<h2 class="titleLight">
+							CONTACT ME
+						</h2>
+						<hr class="separatorLight">
+						<h3 class="coordsTitle">
+							Phone Number
+						</h3>
+						<p class="coordsInfo">
+							(+33) 6 37 63 15 53
+						</p>
+						<hr class="coordsSeparator">
+						<h3 class="coordsTitle">
+							Email Adress
+						</h3>
+						<p class="coordsInfo">
+							jules.baud@julesbaudportfolio.fr
+						</p>
+						<hr class="coordsSeparator">
+						<h3 class="coordsTitle">
+							Postal Adress
+						</h3>
+						<p class="coordsInfo">
+							1 Rue Pierre de Coubertin, 30320, Marguerittes (Fr)
+						</p>
+						<hr class="coordsSeparator">
+					</div>			
+					<div id="socials">
+						<nav id="socialsBar">
+							<ul class="socialsList">
+								<button class="bttnSocials">
+									<a href="https://www.linkedin.com/in/julesbaud/" target="_blank">
+										<img class="iconSocials" src="img/linkedinIcon.png">
+									</a>
+								</button>
+								<button class="bttnSocials">
+									<a href="https://julesbaud.artstation.com/" target="_blank">
+										<img class="iconSocials" src="img/artstationIcon.png">
+									</a>
+								</button>
+								<button class="bttnSocials">
+									<a href="https://www.instagram.com/jbaud_portfolio/" target="_blank">
+										<img class="iconSocials" src="img/instagramIcon.png">
+									</a>
+								</button>
+								<button class="bttnSocials">
+									<a href="mailto:jules.baud@julesbaudportfolio.fr" target="_blank">
+										<img class="iconSocials" src="img/emailIcon.png">
+									</a>
+								</button>
+							</ul>
+						</nav>
+					</div>
+				</div>
+
+				<div class="rightBox">
+					<div class="formBox">
+						<h2 class="titleDark">
+							SEND A MESSAGE
+						</h2>
+						<hr class="separatorDark">
+						<form class="contactForm" method="post">
+							<input type="text" name="firstName" class="name" placeholder="First Name">
+							<input type="text" name="lastName" class="name" placeholder="Last Name">
+							<input type="email" name="email" class="email" placeholder="email@email-exemple.com">
+							<input type="text" name="message" class="message" placeholder="I love your art ! Thank you">
+							<input type="submit" name="submit" class="submitBttn" value="Send Form">
+						</form>							
+					</div>				
+				</div>
+			</div>
+		</div>
+		<div id="footer">
+			<p class="copyright">
+				&copy Jules Baud Portfolio Copyright 2022 - 2023 | All rights reserved | Website powered by HTML & CSS coding &copy
+			</p>
+		</div>
+	</div>
+
